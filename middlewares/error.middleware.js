d80a15ede01fb56fe98f1153be211b09dabecf86
revolution737 <revolutiongamer387@gmail.com>
2026-03-16 11:20:38 +0530

    using middleware to mitigate common errors in mongoose

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,1 +1,17 @@
-const errorMiddleware = (err, req, res, next) 
+const errorMiddleware = (err, req, res, next) => {
+    try {
+        let error = { ...err };
+        error.message = err.message;
+
+        console.error(err);
+
+        //mongoose bad ObjectId
+        if(err.name == 'CastError') {
+            const message = 'Resource not Found';
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+    } catch (error) {
+        next(error);
+    }
+};
