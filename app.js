8a2a258dd7770c2395485afd4ff2063c06ed1f26
revolution737 <revolutiongamer387@gmail.com>
2026-03-16 11:44:25 +0530

    finished setting up custom middleware for better error handling and installed auth packages

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/users.routes.js';
 import subscriptionRouter from './routes/subscriptions.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send("Welcome to the subscription tracker API")
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -11,7 +11,24 @@ const errorMiddleware = (err, req, res, next) => {
             error = new Error(message);
             error.statusCode = 404;
         }
-    } catch (error) {
+
+        if(err.code == 11000) {
+            const message = 'Duplicate field value entered';
+            error.statusCode = 400;
+        }
+
+        if(err.name == 'ValidationError'){
+            const message = Object.values(err.errors).map(val => val.message);
+            error = new Error(message.join(', '));
+            error.statusCode = 400;
+        }
+
+        res.status(error.statusCode || 500).json({ success: false, error: error.message || 'Server Error'});
+    } 
+    
+    catch (error) {
         next(error);
     }
 };
+
+export default errorMiddleware;
