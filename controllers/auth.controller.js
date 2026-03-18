f9d73f91c8846c556c3a2ff830480b1fc114a037
revolution737 <revolutiongamer387@gmail.com>
2026-03-18 23:06:46 +0530

    comment fixes in routes and controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,22 @@
+import mongoose from 'mongoose';
+
 export const signUp = async (req, res, next) => {
-    //sign up logic
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+    // logic to create new user
+    } catch(error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
 }
 
 export const signIn = async (req, res, next) => {
-    //sign up logic
+    //sign in logic
 }
 
 export const signOut = async (req, res, next) => {
-    //sign up logic
+    //sign out logic
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { signIn, signUp, signOut } from '../controllers/authRouter.controller.js';
+import { signIn, signUp, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', signIn)
-authRouter.post('/sign-in', signOut)
-authRouter.post('/sign-out', signUp)
+authRouter.post('/sign-up', signUp)
+authRouter.post('/sign-in', signIn)
+authRouter.post('/sign-out', signOut)
 
 export default authRouter;
